ea6c880e76f0f4965029fd80b9900786428f9b0a
Vladyslav <kovalenko_vladyslav@protonmail.com>
2022-07-31 14:26:56 +0300

    Initial commit from IDEA, removed token from code, added ability to load token from t

--- src/main/java/com/discordbot/Main.java
+++ src/main/java/com/discordbot/Main.java
@@ -1,4 +1,4 @@
-btkc2package com.discordbot;
+package com.discordbot;
 
 import com.sedmelluq.discord.lavaplayer.player.*;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
@@ -16,7 +16,6 @@ import java.net.*;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 import org.javacord.api.audio.*;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.message.Messageable;
@@ -44,14 +43,6 @@ public class Main {
         createSlashCommandListeners(api);
         // Print the invite url of your bot
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
-        try {
-            var commands = api.getGlobalSlashCommands().get();
-            for (var command : commands) {
-                System.out.println(command.getName()+" "+command.getId());
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static EmbedBuilder embedWaifuPics(String link) {
@@ -132,7 +123,7 @@ public class Main {
                 String rawArrayOfHexes = body.substring(body.indexOf("<hex>"),body.lastIndexOf("</hex>")+6);
                 String [] arrayOfHexes = rawArrayOfHexes.split("</hex>");
                 for (int i = 0; i < arrayOfHexes.length; i++) {
-                    arrayOfHexes[i] = arrayOfHexes[i].substring(arrayOfHexes[i].indexOf("<hex>")+5);
+                    arrayOfHexes[i] = arrayOfHexes[i].substring(arrayOfHexes[i].indexOf("<hex>")+6);
                 }
                 EmbedBuilder embedBuilder = new EmbedBuilder()
                         .setTitle(body.substring(body.indexOf("<title>")+16,body.indexOf("</title>")-3))
@@ -185,7 +176,7 @@ public class Main {
 
     public static void createCommands(DiscordApi api) {
         SlashCommand command = SlashCommand.with("waifu", "Send randompic from waifu.in",Arrays.asList(
-                        SlashCommandOption.createBooleanOption("JPEG","JPEG?",false),
+                        SlashCommandOption.createBooleanOption("NSFW","NSFW?",false),
                         SlashCommandOption.createBooleanOption("GIF","GIF?",false)
                 ))
                 .createGlobal(api)
@@ -205,15 +196,12 @@ public class Main {
                 .join();
         // Slash command for waifu.pics
         SlashCommand command1234 = SlashCommand.with("waifupics", "Get random picture from waifu.pics",Arrays.asList(
-                        SlashCommandOption.createBooleanOption("JPEG","JPEG?",false),
+                        SlashCommandOption.createBooleanOption("nsfw","NSFW?",false),
                         SlashCommandOption.createStringOption("tag","Choose tag. Check /help command:waifupics for list of available tags.",false)
                 ))
                 .createGlobal(api)
                 .join();
-        SlashCommand command5678 = SlashCommand.with("colors", "Get Random palette from colourlovers.com")
-                .createGlobal(api)
-                .join();
-        SlashCommand command5679 = SlashCommand.with("stop", "Stop playing radio (WIP)")
+        SlashCommand command5678 = SlashCommand.with("randomcolors", "Get Random palette from colourlovers.com")
                 .createGlobal(api)
                 .join();
     }
@@ -224,10 +212,10 @@ public class Main {
             if (Objects.equals(slashCommandInteraction.getCommandName(), "waifu")) {
                 //waifu listener
                 String link = "https://api.waifu.im/random/?full=false";
-                Boolean JPEGget, gifget;
-                if (slashCommandInteraction.getOptionBooleanValueByName("JPEG").isPresent()) {
-                    JPEGget = slashCommandInteraction.getOptionBooleanValueByName("JPEG").get();
-                    link = link + "&is_JPEG=" + JPEGget;
+                Boolean nsfwget, gifget;
+                if (slashCommandInteraction.getOptionBooleanValueByName("nsfw").isPresent()) {
+                    nsfwget = slashCommandInteraction.getOptionBooleanValueByName("nsfw").get();
+                    link = link + "&is_nsfw=" + nsfwget;
                 }
                 if (slashCommandInteraction.getOptionBooleanValueByName("gif").isPresent()) {
                     gifget = slashCommandInteraction.getOptionBooleanValueByName("gif").get();
@@ -257,14 +245,14 @@ public class Main {
                         EmbedBuilder embedBuilder = new EmbedBuilder()
                                 .setTitle("/waifupics")
                                 .addField("Description:","Get random image, gif or reaction from waifu.pics.")
-                                .addField("Example","/waifupics JPEG:false tag:waifu")
-                                .addField("JPEG options:","false, true")
-                                .addField("JPEG default value:","false")
-                                .addField("Tag options with JPEG = False : ", "waifu, " +
+                                .addField("Example","/waifupics nsfw:false tag:waifu")
+                                .addField("Nsfw options:","false, true")
+                                .addField("Nsfw default value:","false")
+                                .addField("Tag options with NSFW = False : ", "waifu, " +
                                         "neko, shinobu, megumin, bully, cuddle, cry, hug, awoo, kiss, lick, " +
                                         "pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, " +
                                         "nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance")
-                                .addField("Tag options with JPEG = True : ","waifu, neko, trap, blowjob")
+                                .addField("Tag options with NSFW = True : ","waifu, neko, trap, blowjob")
                                 .addField("Tag default option: ","waifu");
                         slashCommandInteraction.createImmediateResponder().addEmbed(embedBuilder).respond();
                     } else {
@@ -278,10 +266,8 @@ public class Main {
                             .addField("/help", "This.")
                             .addField("/help command:text", "Check help for specific command")
                             .addField("/plaza", "Connect to VC and start playing plaza.one")
-                            .addField("/waifu JPEG:false gif:false", "Random picture from waifu.moe.")
-                            .addField("/waifupics JPEG:false tag:text", "Random picture from waifu.pics.")
-                            .addField("/colors", "Random colorpalette from colourlovers.com")
-                            .addField("/playmeltyblood", "What's Melty Blood?")
+                            .addField("/waifu nsfw:false gif:false", "Random picture from waifu.moe.")
+                            .addField("/waifupics nsfw:false tag:text", "Random picture from waifu.pics.")
                             .setFooter("try /help command:waifupics");
                     slashCommandInteraction.createImmediateResponder().addEmbed(embedBuilder).respond();
                 }
@@ -306,32 +292,22 @@ public class Main {
                 //waifu.pics listener
                 String link = "https://api.waifu.pics/";
                 Optional<String> tag = slashCommandInteraction.getOptionStringValueByName("tag");
-                Optional<Boolean> JPEG = slashCommandInteraction.getOptionBooleanValueByName("JPEG");
-                Boolean JPEGget = false;
+                Optional<Boolean> nsfw = slashCommandInteraction.getOptionBooleanValueByName("nsfw");
+                Boolean nsfwget = false;
                 String tagget = "waifu";
-                if (JPEG.isPresent()) JPEGget = JPEG.get();
+                if (nsfw.isPresent()) nsfwget = nsfw.get();
                 if (tag.isPresent()) tagget = tag.get();
-                if (JPEGget) link = link + "JPEG/"; else link = link + "sfw/";
+                if (nsfwget) link = link + "nsfw/"; else link = link + "sfw/";
                 link = link + tagget;
                 slashCommandInteraction.createImmediateResponder().addEmbed(embedWaifuPics(link)).respond();
-            } else if (Objects.equals(slashCommandInteraction.getCommandName(), "colors")) {
-                //colors listener
+            } else if (Objects.equals(slashCommandInteraction.getCommandName(), "randomcolors")) {
+                //randomcolors listener
                 if (slashCommandInteraction.getChannel().isPresent())
                     slashCommandInteraction.createImmediateResponder()
                             .addEmbed(sendColorPalette())
                             .respond();
                 else
                     slashCommandInteraction.createImmediateResponder().setContent("no text channel wtf");
-            } else if (Objects.equals(slashCommandInteraction.getCommandName(), "stop")) {
-                if (slashCommandInteraction.getServer().isPresent()) {
-                    if (slashCommandInteraction.getServer().get().getConnectedVoiceChannel(api.getYourself()).isPresent()) {
-                        slashCommandInteraction
-                                .getServer().get()
-                                .getConnectedVoiceChannel(api.getYourself()).get()
-                                .disconnect();
-                        slashCommandInteraction.createImmediateResponder().setContent("OK.").respond();
-                    } else slashCommandInteraction.createImmediateResponder().setContent("Voice channel not found.").respond();
-                } else slashCommandInteraction.createImmediateResponder().setContent("No group found.").respond();
             }
         });
     }
